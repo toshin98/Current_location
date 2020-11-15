4b983705231eeb5eaed569674f92a48fd27bef32
Toshin Hyanki <toshin@byteblanket.com>
2020-11-15 19:06:24 +0530

    first commit

---
+++ location/App.js
@@ -0,0 +1,173 @@
+import React, {useState, useEffect} from 'react';
+
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  PermissionsAndroid,
+  Platform,
+  Button,
+} from 'react-native';
+
+import Geolocation from '@react-native-community/geolocation';
+
+const App = () => {
+  const [
+    currentLongitude,
+    setCurrentLongitude
+  ] = useState('...');
+  const [
+    currentLatitude,
+    setCurrentLatitude
+  ] = useState('...');
+  const [
+    locationStatus,
+    setLocationStatus
+  ] = useState('');
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      if (Platform.OS === 'ios') {
+        getOneTimeLocation();
+        subscribeLocationLocation();
+      } else {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: 'Location Access Required',
+              message: 'This App needs to Access your location',
+            },
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            getOneTimeLocation();
+            subscribeLocationLocation();
+          } else {
+            setLocationStatus('Permission Denied');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      }
+    };
+    requestLocationPermission();
+    return () => {
+      Geolocation.clearWatch(watchID);
+    };
+  }, []);
+
+  const getOneTimeLocation = () => {
+    setLocationStatus('Getting Location ...');
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setLocationStatus('You are Here');
+
+        const currentLongitude = 
+          JSON.stringify(position.coords.longitude);
+
+        const currentLatitude = 
+          JSON.stringify(position.coords.latitude);
+
+        setCurrentLongitude(currentLongitude);
+        
+        setCurrentLatitude(currentLatitude);
+      },
+      (error) => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 30000,
+        maximumAge: 1000
+      },
+    );
+  };
+
+  const subscribeLocationLocation = () => {
+    watchID = Geolocation.watchPosition(
+      (position) => {
+        
+        setLocationStatus('You are Here');
+        console.log(position);
+
+        const currentLongitude =
+          JSON.stringify(position.coords.longitude);
+
+        const currentLatitude = 
+          JSON.stringify(position.coords.latitude);
+
+        setCurrentLongitude(currentLongitude);
+
+        setCurrentLatitude(currentLatitude);
+      },
+      (error) => {
+        setLocationStatus('Please open GPS');
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 1000
+      },
+    );
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/location.png',
+            }}
+            style={{width: 100, height: 100}}
+          />
+          <Text style={styles.boldText}>
+            {locationStatus}
+          </Text>
+          <Text
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 16,
+            }}>
+            Longitude: {currentLongitude}
+          </Text>
+          <Text
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 16,
+            }}>
+            Latitude: {currentLatitude}
+          </Text>
+          <View style={{marginTop: 20}}>
+            <Button
+              title="Button"
+              onPress={getOneTimeLocation}
+            />
+          </View>
+        </View>
+        
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  boldText: {
+    fontSize: 25,
+    color: 'red',
+    marginVertical: 16,
+  },
+});
+
+export default App;
